04c4a721549f60474512f7d7b7185dc647c0c8c5
marcelohenrique <mhguara@gmail.com>
2014-10-14 23:04:46 -0300

    Começando a trabalhar no cadastro de estudo do usuário.

---
+++ src/test/java/br/com/guarasoft/studyware/estudousuario/casosdeuso/CadastrarEstudoUsuarioTest.java
@@ -0,0 +1,16 @@
+package br.com.guarasoft.studyware.estudousuario.casosdeuso;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CadastrarEstudoUsuarioTest {
+
+	@Ignore
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
